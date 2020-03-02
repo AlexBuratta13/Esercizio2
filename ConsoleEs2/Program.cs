3151d6068f0baf641210de6bb211ae604af3ae1f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-03-02 16:12:37 +0100

    Add project files.

---
+++ ConsoleEs2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Es2Linbr;
+
+namespace ConsoleEs2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci un numero decimale.");
+            try
+            {
+                double a = double.Parse(Console.ReadLine());
+                int n1;
+                if (Utilità.VericaIntero(a))
+                {
+                    Console.WriteLine($"Il numero {a} è intero.");
+                    n1 = (int)a;
+                }
+                else
+                {
+                    Console.WriteLine($"Il numero {a} non è intero.");
+                    n1 = Utilità.Arrotonda(a);
+                }
+                if (Utilità.Pari_Dispari(n1)) 
+                {
+                    Console.WriteLine($"Il numero {n1} è pari.");
+                }
+                else
+                {
+                    Console.WriteLine($"Il numero {n1} è dispari.");
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Si possono inserire solo dei numeri." + e.Message);
+            }
+                Console.ReadLine();
+
+        }
+    }
+}
---
+++ Es2Linbr/Utilità.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es2Linbr
+{
+    public class Utilità
+    { 
+        public static bool VericaIntero(double n)
+        {
+            int a = (int)n;
+            if(a == n)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static int Arrotonda(double c)
+        {
+            int n = (int)(c + 0.5);
+            return n;
+        }
+        public static bool Pari_Dispari(int n1)
+        {
+            if(n1 % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    } 
+}
